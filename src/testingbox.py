5b4379a4f2b114f0bd5b93b6c9c6b8052c65415b
Elia Vicentini <eliavicentini26@gmail.com>
2020-08-08 17:00:39 +0200

    Optimized ctrl libraries

--- src/testingbox.py
+++ src/testingbox.py
@@ -18,16 +18,26 @@ def ctrl_lib():
               "Download it via https://bootstrap.pypa.io/get-pip.py "
               "and start it with 'python3 get-pip.py'.")
 
-    cmd_libraries = subprocess.Popen(['pip3 list'], stdout=subprocess.PIPE)  # Obtaining the list of installed libraries
+    cmd_libraries = subprocess.Popen(['pip3', 'list'], stdout=subprocess.PIPE)  # Obtaining the installed libraries
     i_libraries = str(cmd_libraries.stdout.read())  # Reading the output of the command 'pip3 list'
+    cmd_libraries.stdout.close()
 
     with open('../requirements.txt', 'r') as requirements:
         lines = requirements.readlines()  # Reading every line in 'requirements.txt' and save them in 'lines' array
 
+        not_installed = []
         for i in lines:  # Checking if a required library is installed
             if i not in i_libraries:
-                print(
-                    'Library {0} not installed. \nPlease install this library using \'pip3 install {1}\''.format(i, i))
+                temp = i.replace('\n', '')
+                not_installed.append(temp)
+
+        if len(not_installed) > 1:
+            temp = ""
+            for i in range(len(not_installed)):
+                temp += "'" + not_installed[i] + "' "
+            print("These libraries " + temp + "are not installed. Please install them via pip3")
+        else:
+            print("The library '{0}' is not installed. Please install it via pip3".format(not_installed[0]))
 
 
 if __name__ == '__main__':
