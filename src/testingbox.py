c0b43ee7f1e03d031e4a600638b32a13e90aff42
Elia Vicentini <eliavicentini26@gmail.com>
2020-08-10 14:41:37 +0200

    Added exception in case 'requirements.txt' is not present

--- src/testingbox.py
+++ src/testingbox.py
@@ -26,28 +26,32 @@ def ctrl_lib():
     i_libraries = str(cmd_libraries.stdout.read())  # Reading the output of the command 'pip3 list'
     cmd_libraries.stdout.close()
 
-    with open('../requirements.txt', 'r') as requirements:
-        lines = requirements.readlines()  # Reading every line in 'requirements.txt' and save them in 'lines' array
+    try:
+        with open('../requirements.txt', 'r') as requirements:
+            lines = requirements.readlines()  # Reading every line in 'requirements.txt' and save them in 'lines' array
 
-        not_installed = []
-        for i in lines:  # Checking if a required library is installed
-            if i not in i_libraries:
-                temp = i.replace('\n', '')
-                not_installed.append(temp)
+            not_installed = []
+            for i in lines:  # Checking if a required library is installed
+                if i not in i_libraries:
+                    temp = i.replace('\n', '')
+                    not_installed.append(temp)
 
-        if len(not_installed) > 1:
-            temp = ""
-            for i in range(len(not_installed)):
-                temp += "'" + not_installed[i] + "' "
-            print("These libraries " + temp + "are not installed. Please install them via pip3")
-            exit(1)
-        elif len(not_installed) == 1:
-            print("The library '{0}' is not installed. Please install it via pip3".format(not_installed[0]))
-            exit(1)
-        else:
-            if verbose:
-                print("Libraries are OK")
-            return True
+            if len(not_installed) > 1:
+                temp = ""
+                for i in range(len(not_installed)):
+                    temp += "'" + not_installed[i] + "' "
+                print("These libraries " + temp + "are not installed. Please install them via pip3")
+                exit(1)
+            elif len(not_installed) == 1:
+                print("The library '{0}' is not installed. Please install it via pip3".format(not_installed[0]))
+                exit(1)
+            else:
+                if verbose:
+                    print("Libraries are OK")
+                return True
+    except FileNotFoundError:
+        print("The file 'requirements.txt' has not been found. Check if it is present in the directory or clone the "
+              "repository again from Github")
 
 
 if __name__ == '__main__':
